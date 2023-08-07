5b3b6291b4dde14289268291c971a74897400f70
Renz Haxor <52681808+renzhxr@users.noreply.github.com>
2023-08-08 01:26:41 +0700

    update src/Client.js

--- src/Client.js
+++ src/Client.js
@@ -477,7 +477,6 @@ attributeFilter: ['data-ref'],
 QR_CONTAINER,
 QR_RETRY_BUTTON,
 }
-}
 
 const handleLinkWithPhoneNumber = async () => {
 const LINK_WITH_PHONE_BUTTON = '[data-testid="link-device-qrcode-alt-linking-hint"]';
